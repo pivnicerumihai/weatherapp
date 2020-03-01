672c0adcc6ecde4232624d6fca554715035c4ced
ML Pivniceru <mcpivniceru@gmail.com>
2020-03-01 13:19:56 +0000

    Updates

---
+++ precache-manifest.52d2bd04110fa61a96187b8dacb73cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5d83b603620325cf68224d09721158",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "123d0b713ad778ade583",
+    "url": "/weatherapp/static/css/main.7995c821.chunk.css"
+  },
+  {
+    "revision": "f77b7600a60685114320",
+    "url": "/weatherapp/static/js/2.d371f45b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/weatherapp/static/js/2.d371f45b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "123d0b713ad778ade583",
+    "url": "/weatherapp/static/js/main.8260ef3a.chunk.js"
+  },
+  {
+    "revision": "a071277341b14ea9af15",
+    "url": "/weatherapp/static/js/runtime-main.1f3bab91.js"
+  }
+]);
